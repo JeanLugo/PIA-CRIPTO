78c230dbbccfe007668aa3ec753ffc4cdaa02caa
JeanLugo <47510560+JeanLugo@users.noreply.github.com>
2020-12-05 21:12:45 -0600

    Add files via upload

---
+++ PIA_CRIPTO/src/main/java/PIA/Ejecutar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PIA;
+
+/**
+ *
+ * @author Jean Lugo
+ */
+public class Ejecutar {
+    public static void main(String[] args) {
+        
+        
+        IngresoDatos datos= new IngresoDatos();
+        datos.setVisible(true);
+    }
+}
